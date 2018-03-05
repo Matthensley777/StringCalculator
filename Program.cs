172bc2149b7658456f88941f2bb2324bf4bdabe7
developer <developer@DESKTOP-L75S7D1>
2018-03-05 17:59:45 -0600

    random commit

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NUnit.Framework;
 
 namespace StringCalculator
 {
@@ -10,6 +11,11 @@ namespace StringCalculator
     {
         static void Main(string[] args)
         {
+            //public int CalculatorString { get; set; }
+
+
         }
     }
 }
+
+
